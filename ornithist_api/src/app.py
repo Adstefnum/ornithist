6887d690026b4d2ff34a698d4f1b0836724964a3
Adstefnum <bauerstevenson@gmail.com>
2022-02-21 23:01:08 +0200

    creating overall project structre for model creation, app, and API

---
+++ ornithist_api/src/app.py
@@ -0,0 +1,14 @@
+import flask
+import io
+import string
+import time
+import os
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from flask import Flask, jsonify, request
+
+
+
+# init Flask app
+app = Flask(__name__)
