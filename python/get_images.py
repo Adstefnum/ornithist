d106e5110ea65efb520b6bf65357b01a3204be42
Stephen Adesina <bauerstevenson@gmail.com>
2023-05-23 09:56:25 +0300

    modifying to get all elements

--- python/get_images.py
+++ python/get_images.py
@@ -10,25 +10,28 @@ with open(filename, "r", encoding="utf-8") as file:
     header = next(reader)  # Skip header row
     data = list(reader)
 
-# Update CSV with image links
+# Update CSV with image links and scientific names
 for row in data:
     href = row[1]
     page_response = requests.get(href)
     page_soup = BeautifulSoup(page_response.content, "html.parser")
-    image_element = page_soup.find("img", xpath="/html/body/div[2]/div/div[3]/main/div[3]/div[3]/div[1]/table/tbody/tr[2]/td/a/img")
+    a_element = page_soup.find("a", class_="image")
 
-    if image_element:
-        image_link = image_element["src"]
-        alt_text = image_element.get("alt", "")
-        scientific_name = alt_text.replace("-", "").strip()
+    if a_element:
+        image_tag = a_element.find("img")
+        if image_tag:
+            image_link = "https://en.wikipedia.org" + a_element["href"]
+            alt_text = image_tag.get("alt", "")
+            scientific_name = alt_text.replace("-", "").strip()
+        else:
+            image_link = "N/A"
+            scientific_name = "N/A"
     else:
         image_link = "N/A"
         scientific_name = "N/A"
 
     row.append(image_link)
-    print(image_link)
     row.append(scientific_name)
-    print(scientific_name)
 
 # Save updated data to a new CSV file
 new_filename = "output_with_images.csv"
@@ -37,4 +40,4 @@ with open(new_filename, "w", newline="", encoding="utf-8") as file:
     writer.writerow(header + ["Image Link", "Scientific Name"])
     writer.writerows(data)
 
-print("CSV file updated with image links successfully!")
+print("CSV file updated with image links and scientific names successfully!")
