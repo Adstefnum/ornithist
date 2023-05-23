af25896c77c190fb929945c662fc38dd3ace544f
Stephen Adesina <bauerstevenson@gmail.com>
2023-05-23 10:07:09 +0300

    modified a bit for efficiency

--- python/get_images.py
+++ python/get_images.py
@@ -4,13 +4,18 @@ from bs4 import BeautifulSoup
 
 # Read existing CSV file
 filename = "output.csv"
+new_filename = "output_with_images.csv"
 data = []
-with open(filename, "r", encoding="utf-8") as file:
-    reader = csv.reader(file)
-    header = next(reader)  # Skip header row
-    data = list(reader)
 
-# Update CSV with image links and scientific names
+file = open(filename, "r", encoding="utf-8")
+reader = csv.reader(file)
+header = next(reader)  # Skip header row
+data = list(reader)
+
+file = open(new_filename, "w", newline="", encoding="utf-8")
+writer = csv.writer(file)
+writer.writerow(header + ["Image Link", "Scientific Name"])
+
 for row in data:
     href = row[1]
     page_response = requests.get(href)
@@ -22,7 +27,8 @@ for row in data:
         if image_tag:
             image_link = "https://en.wikipedia.org" + a_element["href"]
             alt_text = image_tag.get("alt", "")
-            scientific_name = alt_text.replace("-", "").strip()
+            scientific_name = alt_text.replace("-", " ").strip()
+            scientific_name = scientific_name.replace(".jpg","")
         else:
             image_link = "N/A"
             scientific_name = "N/A"
@@ -31,13 +37,10 @@ for row in data:
         scientific_name = "N/A"
 
     row.append(image_link)
+    print(image_link)
     row.append(scientific_name)
+    print(scientific_name)
 
-# Save updated data to a new CSV file
-new_filename = "output_with_images.csv"
-with open(new_filename, "w", newline="", encoding="utf-8") as file:
-    writer = csv.writer(file)
-    writer.writerow(header + ["Image Link", "Scientific Name"])
-    writer.writerows(data)
+    writer.writerow(row)
 
 print("CSV file updated with image links and scientific names successfully!")
