f4e14810bae6cf58483d48a256edc921884b9935
Stephen Adesina <bauerstevenson@gmail.com>
2023-05-27 22:40:41 +0300

    script to upload names to appwrite db

--- python/upload_to_appwrite.py
+++ python/upload_to_appwrite.py
@@ -1,50 +1,76 @@
-import requests
+import csv
+from appwrite.client import Client
+from appwrite.services.databases import Databases
+from dotenv import load_dotenv
+import os
+import uuid
+import secrets
+import string
 
-# Replace with your Appwrite endpoint and project ID
-ENDPOINT = 'https://appwrite.example.com/v1'
-PROJECT_ID = 'your-project-id'
+load_dotenv()
+endpoint = os.environ.get('APPWRITE_ENDPOINT')
+project_id = os.environ.get('APPWRITE_PROJECT_ID')
+database_id = os.environ.get('APPWRITE_DATABASE_ID')
+collection_id = os.environ.get('APPWRITE_COLLECTION_ID')
+secret_key = os.environ.get('APPWRITE_SECRET_KEY')
+# Initialize the Appwrite client
+client = Client()
+client.set_endpoint(endpoint) 
+client.set_project(project_id) 
+client.set_key(secret_key)
 
-# Replace with your Appwrite API key
-API_KEY = 'your-api-key'
+# Initialize the Appwrite database service
+db = Databases(client)
+db.get_collection(database_id, collection_id) 
 
-# Set up headers
-headers = {
-    'Content-Type': 'application/json',
-    'X-Appwrite-Project': PROJECT_ID,
-    'X-Appwrite-Key': API_KEY,
-}
+csv_file_path = './output_with_images.csv'
 
-# Replace with the path and filename for your CSV file
-csv_file = 'data.csv'
+failures = open('failures.txt', 'w')
 
-# Replace with the appropriate field names for your data
-fieldnames = ['bird_name','wiki_url','image_link', 'desc']
+def generate_random_id(length):
+    alphabet = string.ascii_letters + string.digits
+    random_id = ''.join(secrets.choice(alphabet) for _ in range(length))
+    return random_id
 
-# Create the CSV file and writer object
-with open(csv_file, mode='w', newline='') as file:
-    writer = csv.DictWriter(file, fieldnames=fieldnames)
-
-    # Write the header row
-    writer.writeheader()
-
-for record in processed_data:
-    with open(csv_file, mode='a', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
-        writer.writerow(record)
-
-    # Upload record to the Appwrite database
-
-    payload = {
-        'name': record['name'],
-        'description': record['description'],
-        'field3': record['field3'],
-        'field4': record['field4'],
-        # Include additional fields as needed
+def prepare_data(row):
+    bird_name = row[bird_name_index]
+    wiki_url = row[wiki_url_index]
+    image_link = row[image_link_index]
+    
+    data = {
+        'bird_name': bird_name,
+        'wiki_url': wiki_url,
+        'image_link': image_link
     }
 
-    response = requests.post(f'{ENDPOINT}/database/collections/<your-collection-id>/documents', headers=headers, json=payload)
+    return data
+ 
 
-    if response.status_code == 201:
-        print('Record created successfully')
-    else:
-        print('Failed to create record:', response.json())
+def upload_to_appwrite(data):
+    document_id = generate_random_id(10)
+    try:
+        response = db.create_document(database_id,collection_id,document_id,data)
+        print(f"Uploaded data for '{data['bird_name']}' - Document ID: {response['$id']}")
+    except:
+        failures.write(f"Failed to upload data for '{data['bird_name']}'")
+
+with open(csv_file_path, 'r') as file:
+    reader = csv.reader(file)
+    headers = next(reader)  # Read the header row
+
+    # Get the index of the required fields
+    bird_name_index = headers.index('name')
+    wiki_url_index = headers.index('wikipedia_link')
+    image_link_index = headers.index('image_link')
+    for row in reader:
+        data = prepare_data(row)
+        upload_to_appwrite(data)
+    
+    
+    failures.close()
+
+
+    
+
+
+
